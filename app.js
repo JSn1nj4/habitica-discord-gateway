bff0b097bc68d828cbda6cb5de02043a47d58344
Elliot Derhay <e.j.derhay@gmail.com>
2019-07-14 18:17:00 -0400

    Start working on idea for controller to handle requests for first route
    Not sure yet about this approach... We'll see.

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 let express = require('express')
 const app = express()
 
+require('./controllers/PartyChat')
+
 app.listen(3000, () => {
   console.log("Server running on port 3000")
 })
 
-app.get('/party/chat/new-message', (req, res, next) => {
-  res.json(['Elliot', 'Katie', 'Nadia'])
+app.post('/party/chat/new-message', (req, res, next) => {
+  
 })
---
+++ controllers/PartyChat.js
@@ -0,0 +1,5 @@
+class PartyChat {
+  
+}
+
+module.exports = PartyChat;
