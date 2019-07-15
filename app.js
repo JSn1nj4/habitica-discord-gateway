01461067269f0d71a322d8b2a42babaa7a0f09be
Elliot Derhay <e.j.derhay@gmail.com>
2019-07-15 13:33:15 -0400

    Offload routing responsibilities to router script

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 // import express from 'express'
-let express = require('express')
-const app = express()
+const app = require('express')()
+const routes = require('./routes')
 
-require('./controllers/PartyChat')
+app.use('/', routes);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000")
-})
-
-app.post('/party/chat/new-message', (req, res, next) => {
-  
 })
