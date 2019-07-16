d4dfdc47561e09a5cfd092281418fd60a227589b
Elliot Derhay <e.j.derhay@gmail.com>
2019-07-16 13:22:45 -0400

    Remove route that's currently useless

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router()
 const partyRouter = require('./party-chat')
 
-router.get('/', (req, res) => {
-  res.sendStatus(200)
-})
-
 router.use('/party/chat', partyRouter)
 
 module.exports = router
