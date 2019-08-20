7873c6fbc5406747df79c40132c3f4eece0f4ca2
Elliot Derhay <e.j.derhay@gmail.com>
2019-08-20 13:36:05 -0400

    Duh, the app port should depend on environment...

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // import express from 'express'
 const app = require('express')()
 const routes = require('./routes')
+const port = process.env.PORT || 3000;
 
 app.use('/', routes);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000")
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
