c1e51a8d0d247a41f56436b9815367821c71922d
Elliot Derhay <e.j.derhay@gmail.com>
2019-08-20 13:06:43 -0400

    Try adding simple HTTP endpoint
    This is only to confirm the app is accessible on the web.

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ const partyRouter = require('./party-chat')
 
 router.use('/party/chat', partyRouter)
 
+router.get('/', (req, res) => res.json({status: 200, message: 'Hello world!'}));
+
 module.exports = router
