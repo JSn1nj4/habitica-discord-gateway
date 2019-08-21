5c1ed54ffe9a8c67fdd34ae635e48db75c026e80
Elliot Derhay <e.j.derhay@gmail.com>
2019-08-21 14:11:29 -0400

    Implement body-parser

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // import express from 'express'
 const app = require('express')()
 const Sentry = require("@sentry/node")
+const bodyParser = require('body-parser')
 const routes = require('./routes')
 const port = process.env.PORT || 80;
 
 Sentry.init({ dsn: `https://${ process.env.SENTRY_KEY }@sentry.io/1537673` })
 
 app.use(Sentry.Handlers.requestHandler())
-app.use('/', routes);
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+
+app.use('/', routes)
 app.use(Sentry.Handlers.errorHandler())
 
 app.listen(port, () => {
