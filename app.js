452738e2e4a70d96f5479f174c49d09d7859c320
Elliot Derhay <e.j.derhay@gmail.com>
2019-08-21 13:46:35 -0400

    Register Sentry for logging

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // import express from 'express'
 const app = require('express')()
+const Sentry = require("@sentry/node")
 const routes = require('./routes')
 const port = process.env.PORT || 80;
 
+Sentry.init({ dsn: `https://${ process.env.SENTRY_KEY }@sentry.io/1537673` })
+
+app.use(Sentry.Handlers.requestHandler())
 app.use('/', routes);
+app.use(Sentry.Handlers.errorHandler())
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
