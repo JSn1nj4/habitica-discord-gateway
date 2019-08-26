3694944f0a731e2702d1279f203548587763773b
Elliot Derhay <e.j.derhay@gmail.com>
2019-08-26 13:49:30 -0400

    Change controller to log some request body parts
    This is to log some objects that are too far down for console.log to
    actually dig into them.

--- routes/controllers/PartyChatController.js
+++ routes/controllers/PartyChatController.js
@@ -13,7 +13,34 @@ class PartyChatController {
   }
 
   async sendMessage(req) {
-    console.log(req.body)
+    // console.log(req.body)
+    console.log('Request body pieces:');
+    let body = req.body;
+
+    if(body.info && body.info !== {}) {
+      console.log('body.info:');
+      console.log(body.info);
+    }
+
+    if(body.likes && body.likes !== {}) {
+      console.log('body.likes:');
+      console.log(body.likes);
+    }
+
+    if(body.contributor && body.contributor !== {}) {
+      console.log('body.contributor:');
+      console.log(body.contributor);
+    }
+
+    if(body.backer && body.backer !== {}) {
+      console.log('body.backer:');
+      console.log(body.backer);
+    }
+
+    if(body.userStyles && body.userStyles !== {}) {
+      console.log('body.userStyles:');
+      console.log(body.userStyles);
+    }
   }
 }
 
