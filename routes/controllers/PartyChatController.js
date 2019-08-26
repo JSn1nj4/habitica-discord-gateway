e55f6f16b3de3297c459ed11d8a860bd986e0c38
Elliot Derhay <e.j.derhay@gmail.com>
2019-08-26 14:29:36 -0400

    Go back to logging entire request body

--- routes/controllers/PartyChatController.js
+++ routes/controllers/PartyChatController.js
@@ -13,34 +13,7 @@ class PartyChatController {
   }
 
   async sendMessage(req) {
-    // console.log(req.body)
-    console.log('Request body pieces:');
-    let chat = req.body.chat;
-
-    if(chat.info && chat.info !== {}) {
-      console.log('chat.info:');
-      console.log(chat.info);
-    }
-
-    if(chat.likes && chat.likes !== {}) {
-      console.log('chat.likes:');
-      console.log(chat.likes);
-    }
-
-    if(chat.contributor && chat.contributor !== {}) {
-      console.log('chat.contributor:');
-      console.log(chat.contributor);
-    }
-
-    if(chat.backer && chat.backer !== {}) {
-      console.log('chat.backer:');
-      console.log(chat.backer);
-    }
-
-    if(chat.userStyles && chat.userStyles !== {}) {
-      console.log('chat.userStyles:');
-      console.log(chat.userStyles);
-    }
+    console.log(req.body)
   }
 }
 
