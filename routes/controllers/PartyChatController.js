aa7525ea957269ef25f4e9d0b04ccc7b56927e29
Elliot Derhay <e.j.derhay@gmail.com>
2019-08-29 14:37:32 -0400

    First attempt at writing data to endpoint
    MAKE SURE TO ADD VALUES FOR NEW ENVIRONMENT VARS!

--- routes/controllers/PartyChatController.js
+++ routes/controllers/PartyChatController.js
@@ -1,5 +1,6 @@
 const { Sentry } = require('../../config')
 const messageSchemas = require('../../validators/party/chat/messages')
+const https = require('https')
 
 class PartyChatController {
   constructor() {
@@ -25,6 +26,47 @@ class PartyChatController {
   sendMessage(data) {
     console.log('Validated body object: ')
     console.log(data)
+
+    let msg = JSON.stringify({
+      content: "New party message!",
+      embeds: [
+        {
+          author: {
+            name: data.chat.username || data.chat.uuid
+          },
+          title: `${data.group.name} party chat`,
+          text: data.chat.text,
+          url: "https://habitica.com/party",
+          color: 4401268,
+          timestamp: data.chat.timestamp
+        }
+      ]
+    })
+
+    // either wrap this in its own class or replace with Axios
+    const options = {
+      hostname: 'discordapp.com',
+      port: 443,
+      path: `/api/webhooks/${process.env.PARTY_CHAT_WEBHOOK_ID}/${process.env.PARTY_CHAT_WEBHOOK_TOKEN}`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': msg.length
+      }
+    }
+
+    const req = https.request(options, (res) => {
+      console.log(`statusCode ${res.statusCode}`)
+
+      res.on('data', (d) => {
+        process.stdout.write(d)
+      })
+    })
+
+    req.on('error', this.logError)
+
+    req.write(msg);
+    req.end();
   }
 
   logError(error) {
